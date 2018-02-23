86f61e89f50628bd975e15dbcbe6348efdca5554
xiaotao <xiaotao@faceu>
2018-02-23 11:24:47 +0800

    修改异常退出的输出方式

--- runtime/lar_booter.go
+++ runtime/lar_booter.go
@@ -1,15 +1,19 @@
 import (
+    "fmt"
     "os"
 )
+
+func lar_booter_exit_with_catched(c *lar_gcls_inst_10___builtins_7_Catched_1_lar_intf_10___builtins_9_Throwable) {
+    fmt.Fprintln(os.Stderr, "\033[40;31m" + lar_util_convert_lar_str_to_go_str(c.m_traceback) + "\033[0m")
+    os.Exit(2)
+}
 
 func lar_booter_start_prog(main_mod_init_func func (), main_func func (*[]*lar_cls_10___builtins_6_String) int32,
                            argv *[]*lar_cls_10___builtins_6_String) int {
     defer func () {
         c := lar_func_10___builtins_10_catch_base(recover())
         if c != nil {
-            println("程序异常退出：")
-            println(lar_util_convert_lar_str_to_go_str(c.m_traceback))
-            os.Exit(2)
+            lar_booter_exit_with_catched(c)
         }
     }()
     lar_env_init_mod___builtins()
@@ -21,9 +25,7 @@ func lar_booter_start_co(co lar_intf_10___builtins_9_Coroutine) {
     defer func () {
         c := lar_func_10___builtins_10_catch_base(recover())
         if c != nil {
-            println("程序异常退出：")
-            println(lar_util_convert_lar_str_to_go_str(c.m_traceback))
-            os.Exit(2)
+            lar_booter_exit_with_catched(c)
         }
     }()
     co.method_run()
