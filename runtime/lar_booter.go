af06df4fd56510a4b27ed3df203349f05b72b0b0
xiaotao <xiaotao@faceu>
2018-06-24 14:30:16 +0800

    程序crash的时候输出larva的traceback

--- runtime/lar_booter.go
+++ runtime/lar_booter.go
@@ -8,8 +8,18 @@ func lar_booter_exit_with_catched(c *lar_gcls_inst_10___builtins_7_Catched_1_lar
     os.Exit(2)
 }
 
+func lar_booter_check_go_panic() {
+    r := recover()
+    if r != nil {
+        fmt.Println("process crash, traceback:")
+        fmt.Println(lar_str_to_go_str(lar_exc_create_catched_throwable(nil, 5).m_tb))
+        panic(r)
+    }
+}
+
 func lar_booter_start_prog(main_mod_init_func func (), main_func func (*lar_arr_lar_cls_10___builtins_6_String_1) int32,
                            argv *lar_arr_lar_cls_10___builtins_6_String_1) int {
+    defer lar_booter_check_go_panic()
     defer func () {
         c := lar_func_10___builtins_10_catch_base(recover())
         if c != nil {
@@ -22,6 +32,7 @@ func lar_booter_start_prog(main_mod_init_func func (), main_func func (*lar_arr_
 }
 
 func lar_booter_start_co(co lar_intf_10___builtins_9_Coroutine) {
+    defer lar_booter_check_go_panic()
     defer func () {
         c := lar_func_10___builtins_10_catch_base(recover())
         if c != nil {
