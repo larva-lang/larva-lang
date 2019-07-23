0d5af1eb58f7ccf81252dcf5a4710e21aa13241c
sriverxiao <691515082@qq.com>
2019-07-23 12:03:26 +0800

    example和test移动到标准库中

--- test/syntax/bad_case/import_module/relative_import/path_to_other_module/run.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-
-curr_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-os.chdir(curr_dir + "/..")
-dir_suffix = "/test/syntax/bad_case/relative_import/path_to_other_module"
-assert curr_dir.endswith(dir_suffix)
-larva_dir = curr_dir[: -len(dir_suffix)]
-os.system("python %s/compiler/larc.py --module_path=. path_to_other_module" % larva_dir)
